4e70681210caea32a8b77d67d960972bacd09077
Om Dhanwant <>
2020-09-19 10:34:17 +0530

    --Home  banner UI

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,21 +17,27 @@
                 <span class="sr-only">Next</span>
             </a>
             </div>
+            
+            <div class="banner-text">
+                <span >It Support Website</span>
+            </div>
+
+            
         </div>
        
     </div>
 </template>
 
 <script>
-// import bbqImage from '../assets/bbq.jpg';
-import defaultImage from '../assets/default.png';
+import bbqImage from '../assets/bbq.jpg';
+// import defaultImage from '../assets/default.png';
 export default {
       data(){
        return {
            corouselItems: [
-               { name: '1' , url: defaultImage , active: true },
-               { name: '2' , url: defaultImage , active: false },
-               { name: '3' , url: defaultImage , active: false }
+               { name: '1' , url: bbqImage , active: true },
+               { name: '2' , url: bbqImage , active: false },
+               { name: '3' , url: bbqImage , active: false }
            ]
        }
     }
@@ -51,10 +57,33 @@ export default {
 
 .carousel-item {
     height: 500px;
+    opacity: 0.5;
+}
+
+.banner-text{
+    position: absolute;
+    top: 0;
+    margin: 192px auto;
+    padding: 20px 154px;
+    background: #fff;
+    opacity: 0.8;
+    width: 100%;
+    border-radius: 5px;
+    text-align: center;
+}
+
+.banner span{
+    color: gray;
+    font-size: 50px;
 }
 
 .carousel-item img {
     object-fit: cover!important;
     height: 500px!important;
+}
+
+.carousel-control-prev, .carousel-control-next{
+    height: 500px;
+    opacity: 1!important;
 }
 </style>
