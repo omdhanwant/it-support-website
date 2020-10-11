fec51f363ed587bfa9bc06c061886dbf4e7bc1a2
Om Dhanwant <>
2020-10-11 13:34:54 +0530

    footer UI changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,88 +1,101 @@
 <template>
-<div>
-<footer class="templateux-footer bg-light">
-  <div class="container">
-
-    <div class="row mb-5 p-3">
-      <div class="col-md-4 pr-md-5">
-        <div class="block-footer-widget">
-          <h3>About</h3>
-          <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-        </div>
-      </div>
-
-      <div class="col-md-8">
-        <div class="row">
-          <div class="col-md-3">
+  <div>
+    <footer class="templateux-footer bg-light">
+      <div class="container">
+        <div class="row mb-5 p-3">
+          <!-- <div class="col-md-4 pr-md-5">
             <div class="block-footer-widget">
-              <h3>Learn More</h3>
-              <ul class="list-unstyled">
-                <li><a href="#">How it works?</a></li>
-                <li><a href="#">Useful Tools</a></li>
-                <li><a href="#">Pricing</a></li>
-                <li><a href="#">Sitemap</a></li>
-              </ul>
+              <h3>About</h3>
+              <p>
+                A small river named Duden flows by their place and supplies it
+                with the necessary regelialia.
+              </p>
             </div>
-          </div>
-          <div class="col-md-3">
-            <div class="block-footer-widget">
-              <h3>Support</h3>
-              <ul class="list-unstyled">
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Contact Us</a></li>
-                <li><a href="#">Help Desk</a></li>
-                <li><a href="#">Knowledgebase</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="block-footer-widget">
-              <h3>About Us</h3>
-              <ul class="list-unstyled">
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Careers</a></li>
-                <li><a href="#">Terms of Service</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-              </ul>
-            </div>
-          </div>
+          </div> -->
 
-          <div class="col-md-3">
-            <div class="block-footer-widget">
-              <h3>Connect With Us</h3>
-              <ul class="list-unstyled block-social">
+          <!-- <div class="col-md-12">
+            <div class="row"> -->
+              <div class="col-md-3">
+                <div class="block-footer-widget">
+                  <h3>Learn More</h3>
+                  <ul class="list-unstyled">
+                    <li><a href="#">How it works?</a></li>
+                    <li><a href="#">Useful Tools</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">Sitemap</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="block-footer-widget">
+                  <h3>Support</h3>
+                  <ul class="list-unstyled">
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Help Desk</a></li>
+                    <li><a href="#">Knowledgebase</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="block-footer-widget">
+                  <h3>About Us</h3>
+                  <ul class="list-unstyled">
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Terms of Service</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="col-md-3">
+                <div class="block-footer-widget">
+                  <h3>Connect With Us</h3>
+                  <!-- <ul class="list-unstyled block-social">
                 <li><a href="#" class="p-1"><span class="icon-facebook-square"></span></a></li>
                 <li><a href="#" class="p-1"><span class="icon-twitter"></span></a></li>
                 <li><a href="#" class="p-1"><span class="icon-github"></span></a></li>
-              </ul>
-            </div>
+              </ul> -->
+                  <div style="display: flex; flex-direction: column;">
+                    <p>Email: <a href="kumarbsindia@gmail.com">kumarbsindia@gmail.com</a></p>
+                    <p>Phone: +1(817) 402-8792</p>
+                  </div>
+                </div>
+              </div>
+            <!-- </div> -->
+            <!-- .row -->
+          <!-- </div> -->
+        </div>
+        <!-- .row -->
+
+        <div class="row text-center">
+          <div class="col-md-12 text-center">
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright © All rights reserved
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
           </div>
-        </div> <!-- .row -->
-
+        </div>
+        <!-- .row -->
       </div>
-    </div> <!-- .row -->
-
-    <div class="row text-center">
-      <div class="col-md-12 text-center"><p>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright © All rights reserved  
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-      </p></div>
-    </div> <!-- .row -->
-
+    </footer>
   </div>
-</footer>
-</div>
-
 </template>
 
 <style scoped>
-
-.block-footer-widget h3{
-	font-size: var(--fs-h3)!important;
+.templateux-footer {
+  box-shadow: 10px 5px 10px 8px #e6e6e6;
+}
+.block-footer-widget h3 {
+  font-size: var(--fs-h3) !important;
 }
 
-.block-footer-widget ul>li>a {
-	color: slateblue!important;
+.block-footer-widget ul > li > a, p {
+  color: var(--clr-primary) !important;
+}
+.block-footer-widget p {
+    margin: 0;
 }
 </style>
