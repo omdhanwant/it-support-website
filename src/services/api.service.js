f2d9ff6acd9d4e2c416f37dab379974a9bcbf499
Om Dhanwant <>
2020-10-27 20:06:07 +0530

    -- dynamic host change

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const api_base_url = 'http://localhost:3000'; /// window.location.origin.includes('localhost')
-
+const api_base_url = window.location.origin.includes('localhost') ? 'http://localhost:3000' : 'https://kbshealthcare.com'
+// https://kbshealthcare.com/
 export default {
     sendMail(payload) {
         const url = `${api_base_url}/itsupport/send-mail`
